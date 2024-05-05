6ddd07ed75945f942fe691532e8aab59f7a84178
Mohsen <83723914+mohsenrahmdeli@users.noreply.github.com>
2024-05-05 10:57:23 +0400

    Inintialize Repostory

---
+++ main.py
@@ -0,0 +1,10 @@
+from unpy2exe import decompile
+
+# Path to the exe file
+exe_path = 'path_to_your_exe_file.exe'
+
+# Path to save the decompiled files
+output_dir = 'path_to_save_decompiled_files/'
+
+# Decompile the exe file
+decompile(exe_path, output_dir)
